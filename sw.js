ef47e82155767060f66dac3b030f255522d6c2d0
eslamnetplusy-code <eslamnetplusy@gmail.com>
2026-01-27 20:58:02 +0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("islam-net-plus-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./updates.html",
+        "./logo.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
